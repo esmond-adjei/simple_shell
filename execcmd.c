1849ef29c625e54a25ec480239433d053fb973d8
esmond-adjei <xmongram@gmail.com>
2023-02-22 10:49:36 -0800

    modified some files

--- execcmd.c
+++ execcmd.c
@@ -5,28 +5,27 @@
 
 int execute_cmd(char **args)
 {
-    pid_t pid;
-    int status;
+	pid_t pid;
+	int status;
 
-    pid = fork();
-    if (pid == CHILDPROCESS)
-    {
-        /* Child process */
-        if (execvp(args[0], args) == -1)
-        {
-            perror("command error: "); /* COULDN'T EXECUTE ARGUMENT */
-        }
-        exit(EXIT_FAILURE);
-    }
-    else if (pid == -1)
-    {
-        perror("fork error: ");
-    }
-    else
-    {
-        /* Parent process */
-        wait(&status);
-    }
-
-    return 1;
+	pid = fork();
+	if (pid == CHILDPROCESS)
+	{
+		/* Child process */
+		if (execvp(args[0], args) == -1)
+		{
+			perror("command error: "); /* COULDN'T EXECUTE ARGUMENT */
+		}
+		exit(EXIT_FAILURE);
+	}
+	else if (pid == -1)
+	{
+		perror("fork error: ");
+	}
+	else
+	{
+		/* Parent process */
+		wait(&status);
+	}
+	return (1);
 }
--- tokenizestr.c
+++ tokenizestr.c
@@ -9,37 +9,37 @@
  */
 char **tokenize_string(char *str)
 {
-    int bufsize = TOKEN_BUFSIZE, position = 0;
-    char **tokens = malloc(bufsize * sizeof(char *));
-    char *token, **tokens_backup;
+	int bufsize = TOKEN_BUFSIZE, position = 0;
+	char **tokens = malloc(bufsize * sizeof(char *));
+	char *token, **tokens_backup;
 
-    if (!tokens)
-    {
-        fprintf(stderr, "hsh: allocation error\n");
-        exit(EXIT_FAILURE);
-    }
+	if (!tokens)
+	{
+		fprintf(stderr, "hsh: allocation error\n");
+		exit(EXIT_FAILURE);
+	}
 
-    token = strtok(str, TOKEN_DELIM);
-    while (token != NULL)
-    {
-        tokens[position] = token;
-        position++;
+	token = strtok(str, TOKEN_DELIM);
+	while (token != NULL)
+	{
+		tokens[position] = token;
+		position++;
 
-        if (position >= bufsize)
-        {
-            bufsize += TOKEN_BUFSIZE;
-            tokens_backup = tokens;
-            tokens = realloc(tokens, bufsize * sizeof(char *));
-            if (!tokens)
-            {
-                free(tokens_backup);
-                fprintf(stderr, "lsh: allocation error\n");
-                exit(EXIT_FAILURE);
-            }
-        }
+		if (position >= bufsize)
+		{
+			bufsize += TOKEN_BUFSIZE;
+			tokens_backup = tokens;
+			tokens = realloc(tokens, bufsize * sizeof(char *));
+			if (!tokens)
+			{
+				free(tokens_backup);
+				fprintf(stderr, "hsh: allocation error\n");
+				exit(EXIT_FAILURE);
+			}
+		}
 
-        token = strtok(NULL, TOKEN_DELIM);
-    }
-    tokens[position] = NULL;
-    return tokens;
+		token = strtok(NULL, TOKEN_DELIM);
+	}
+	tokens[position] = NULL;
+	return (tokens);
 }
